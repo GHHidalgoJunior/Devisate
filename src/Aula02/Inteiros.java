9066a9158ac4e4bc2b5ea89fe9cf6c8b6b6447ed
hidalgo <hidalgo@Peniel>
2016-02-19 13:27:07 -0200

    Verificando...

---
+++ src/Aula02/Inteiros.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Aula02;
+
+/**
+ *
+ * @author hidalgo
+ */
+public class Inteiros {
+
+    public static void main(String args[]) {
+        byte varByte = 127;
+        short varShort = 32000;
+        int varInt = 20000000;
+        long varLong = 300000000;
+
+        System.out.println("Byte: " + varByte);
+        System.out.println("Short: " + varShort);
+        System.out.println("Int: " + varInt);
+        System.out.println("Long: " + varLong);
+
+    }
+}
